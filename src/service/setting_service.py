2ba4ba887acc929e34f9dbfb2df27c5d23d3ce05
parkhs21 <2001phs92@gmail.com>
2024-04-25 14:07:04 +0900

    feat: setting tab 초기 로딩 구현

--- src/service/setting_service.py
+++ src/service/setting_service.py
@@ -6,18 +6,46 @@ class SettingService:
     
     def __init__(self, session: MySession):
         self.session = session
-
-    def reload_info(self):
+        
+    def initial_render(self):
         try:
-            response: MyResponse = self.session.get_model_infos()
+            model_infos_res: MyResponse = self.session.get_model_infos()
+            model_cached_res: MyResponse = self.session.get_model_cached()
+            gpu_list_res = [
+                "A6000",
+                "A5500",
+                "A5000",
+                "A4000"
+            ]
             
             gpu_info = {}
-            for info in response.result["gpu_info"]:
+            for info in model_infos_res.result["gpu_info"]:
                 name = f'{info["name"]} ({info["memory.used [MiB]"]} / {info["memory.total [MiB]"]} MiB)'
                 confidence = int(info["memory.used [MiB]"])/int(info["memory.total [MiB]"])
                 gpu_info[name] = confidence
             
-            return gpu_info, response.result["model_loaded_list"]
+            return [
+                gpu_info,
+                model_infos_res.result["model_loaded_list"],
+                gr.update(choices=model_cached_res.result, value=model_cached_res.result[0]),
+                gr.update(choices=gpu_list_res, value=gpu_list_res[0]),
+            ]
+        except MyException as e:
+            raise gr.Error(e.msg)
+        except:
+            raise gr.Error("네트워크 통신에 실패했습니다.")
+
+    def reload_info(self):
+        try:
+            res: MyResponse = self.session.get_model_infos()
+            
+            gpu_info = {}
+            for info in res.result["gpu_info"]:
+                name = f'{info["name"]} ({info["memory.used [MiB]"]} / {info["memory.total [MiB]"]} MiB)'
+                confidence = int(info["memory.used [MiB]"])/int(info["memory.total [MiB]"])
+                gpu_info[name] = confidence
+            
+            return gpu_info, res.result["model_loaded_list"]
         except MyException as e:
             raise gr.Error(e.msg)
         except:
@@ -26,15 +54,15 @@ class SettingService:
 
     def model_load(self, model: str, gpu: str):
         try:
-            response: MyResponse = self.session.post_model_load(model, gpu)
+            res: MyResponse = self.session.post_model_load(model, gpu)
             
             gpu_info = {}
-            for info in response.result["gpu_info"]:
+            for info in res.result["gpu_info"]:
                 name = f'{info["name"]} ({info["memory.used [MiB]"]} / {info["memory.total [MiB]"]} MiB)'
                 confidence = int(info["memory.used [MiB]"])/int(info["memory.total [MiB]"])
                 gpu_info[name] = confidence
             
-            return gpu_info, response.result["model_loaded_list"]
+            return gpu_info, res.result["model_loaded_list"]
         except MyException as e:
             raise gr.Error(e.msg)
         except:
--- src/tab/setting_tab.py
+++ src/tab/setting_tab.py
@@ -16,46 +16,57 @@ class SettingTab:
     def __init__(self, session: MySession):
         self.service = SettingService(session)
         with gr.Tab("설정") as self.tab:
-            gr.Markdown("- LLM 모델과 관련된 설정을 할 수 있습니다.")
-            gr.Markdown("- 모델은 로드된 경우에만 사용 가능합니다.")
-            gr.Markdown("- 30분 동안 사용되지 않은 모델은 자동으로 언로드 됩니다.")
-            gr.Markdown("- Model 명칭은 [Hugging Face]에서 추출하였습니다.")
-            
-            self.gpu_usage_label = gr.Label(
-                label="GPU 점유량",
-                elem_id="gpu_graph"
-            )
-            
-            self.model_loaded_hltext = gr.HighlightedText(
-                # [["Nothing loaded\n", None]],
-                label="Model Load 현황",
-                combine_adjacent=True,
-                adjacent_separator='',
-                color_map={"GPU A6000": "orange", "GPU A5500": "orange", "GPU A5000": "orange", "GPU A4000": "orange"},
-                elem_id="model_load_state"
-            )
-            
-            with gr.Row():
-                self.model_load_dd = gr.Dropdown(
-                    # model_list,
-                    label="Model",
-                    # value=model_list[0],
-                    interactive=True,
-                    scale=4
+            with gr.Blocks() as block:
+                gr.Markdown("- LLM 모델과 관련된 설정을 할 수 있습니다.")
+                gr.Markdown("- 모델은 로드된 경우에만 사용 가능합니다.")
+                gr.Markdown("- 30분 동안 사용되지 않은 모델은 자동으로 언로드 됩니다.")
+                gr.Markdown("- Model 명칭은 [Hugging Face]에서 추출하였습니다.")
+                
+                self.gpu_usage_label = gr.Label(
+                    label="GPU 점유량",
+                    elem_id="gpu_graph"
                 )
                 
-                self.gpu_load_dd = gr.Dropdown(
-                    # gpu_list,
-                    label="GPU",
-                    # value=gpu_list[1],
-                    interactive=True
+                self.model_loaded_hltext = gr.HighlightedText(
+                    # [["Nothing loaded\n", None]],
+                    label="Model Load 현황",
+                    combine_adjacent=True,
+                    adjacent_separator='',
+                    color_map={"GPU A6000": "orange", "GPU A5500": "orange", "GPU A5000": "orange", "GPU A4000": "orange"},
+                    elem_id="model_load_state"
                 )
                 
-            with gr.Row():
-                # self.unload_btn = gr.Button("Unload")
-                self.refresh_btn = gr.Button("Refresh")
-                self.load_btn = gr.Button("Load", variant="primary")
+                with gr.Row():
+                    self.model_load_dd = gr.Dropdown(
+                        # model_list,
+                        label="Model",
+                        # value=model_list[0],
+                        interactive=True,
+                        scale=4
+                    )
+                    
+                    self.gpu_load_dd = gr.Dropdown(
+                        # gpu_list,
+                        label="GPU",
+                        # value=gpu_list[1],
+                        interactive=True
+                    )
+                    
+                with gr.Row():
+                    # self.unload_btn = gr.Button("Unload")
+                    self.refresh_btn = gr.Button("Refresh")
+                    self.load_btn = gr.Button("Load", variant="primary")
 
+
+            block.load(
+                fn=self.service.initial_render,
+                outputs=[
+                    self.gpu_usage_label,
+                    self.model_loaded_hltext,
+                    self.model_load_dd,
+                    self.gpu_load_dd
+                ]
+            )
 
             self.refresh_btn.click(
                 fn=self.service.reload_info,
