94044449a28ca837742ecf412329cf7fc6f696e6
parkhs21 <2001phs92@gmail.com>
2024-06-13 16:31:06 +0900

    feat: 도커 파일 추가

--- web/index.py
+++ web/index.py
@@ -4,5 +4,4 @@ main_block = MainBlock()
 demo = main_block.block
 
 if __name__=='__main__':
-    demo.queue(default_concurrency_limit=5)
-    demo.launch(server_name="0.0.0.0", server_port=7863)
+    demo.queue().launch()
